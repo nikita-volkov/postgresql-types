4cc9c05be631f23695fa3a56c1f713e91fe804d4
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-08-19 21:35:15 +0300

    Remove Vias from exports

--- src/primitive-layer/PrimitiveLayer.hs
+++ src/primitive-layer/PrimitiveLayer.hs
@@ -1,10 +1,8 @@
 module PrimitiveLayer
   ( module PrimitiveLayer.Algebra,
     module PrimitiveLayer.Primitives,
-    module PrimitiveLayer.Vias,
   )
 where
 
 import PrimitiveLayer.Algebra
 import PrimitiveLayer.Primitives
-import PrimitiveLayer.Vias
