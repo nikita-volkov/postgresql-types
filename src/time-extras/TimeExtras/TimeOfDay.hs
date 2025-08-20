c2f302db9fcfe8c72545978753c2f0de2d298e57
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-08-20 22:49:19 +0300

    Add utils

--- src/time-extras/TimeExtras/TimeOfDay.hs
+++ src/time-extras/TimeExtras/TimeOfDay.hs
@@ -1,5 +1,6 @@
 module TimeExtras.TimeOfDay where
 
+import Control.Monad
 import Data.Fixed
 import Data.Time
 import Prelude
@@ -17,3 +18,11 @@ compressToMicroseconds (TimeOfDay hours minutes picoseconds) =
       microseconds = div picoseconds' 1_000_000
       microseconds' = fromIntegral ((hours * 60 + minutes) * 60) * 1_000_000 + fromIntegral microseconds
    in microseconds'
+
+compileToMicroseconds :: TimeOfDay -> Maybe Int
+compileToMicroseconds (TimeOfDay hours minutes picoseconds) = do
+  let MkFixed picoseconds' = picoseconds
+      (microseconds, remainder) = divMod picoseconds' 1_000_000
+  guard (remainder == 0)
+  let microseconds' = fromIntegral ((hours * 60 + minutes) * 60) * 1_000_000 + fromIntegral microseconds
+  pure microseconds'
