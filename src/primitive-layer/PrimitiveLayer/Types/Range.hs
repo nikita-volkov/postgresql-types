4f90e13e810174571462712a93ebc45d9c3569f3
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-08-23 02:08:53 +0300

    Normalize and fix

--- src/primitive-layer/PrimitiveLayer/Types/Range.hs
+++ src/primitive-layer/PrimitiveLayer/Types/Range.hs
@@ -11,17 +11,9 @@ import qualified TextBuilder
 
 data Range a
   = EmptyRange
-  | BoundedRange (RangeBound a) (RangeBound a)
+  | BoundedRange (Maybe a) (Maybe a)
   deriving stock (Eq, Functor)
   deriving (Show) via (ViaPrimitive (Range a))
-
-data RangeBound a = RangeBound
-  { -- | Whether the bound is inclusive or exclusive.
-    inclusive :: Bool,
-    -- | Nothing when infinity.
-    value :: Maybe a
-  }
-  deriving stock (Eq, Show, Functor)
 
 instance (RangeMapping a, Ord a) => Mapping (Range a) where
   typeName = retag @a rangeTypeName
@@ -30,13 +22,10 @@ instance (RangeMapping a, Ord a) => Mapping (Range a) where
   binaryEncoder = \case
     EmptyRange ->
       Write.word8 0b00000001
-    BoundedRange (RangeBound lowerInclusive lowerValue) (RangeBound upperInclusive upperValue) ->
+    BoundedRange lowerValue upperValue ->
       mconcat
         [ Write.word8
-            ( 0
-                .|. (if lowerInclusive then 0b00000010 else 0)
-                .|. (if upperInclusive then 0b00000100 else 0)
-                .|. (if null lowerValue then 0b00001000 else 0)
+            ( (if null lowerValue then 0b00001000 else 0b00000010)
                 .|. (if null upperValue then 0b00010000 else 0)
             ),
           foldMap renderBound lowerValue,
@@ -59,15 +48,20 @@ instance (RangeMapping a, Ord a) => Mapping (Range a) where
       else do
         lowerValue <- decodeBound lowerInfinite
         upperValue <- decodeBound upperInfinite
-        if upperValue == lowerValue
-          then when (not (lowerInclusive && upperInclusive)) do
-            throwError (DecodingError ["range"] (InvalidValueDecodingErrorReason "When lower and upper bounds are equal, both must be inclusive" (TextBuilder.toText (TextBuilder.decimal flags))))
-          else case (lowerValue, upperValue) of
-            (Just lv, Just uv) ->
-              when (lv > uv) do
-                throwError (DecodingError ["range"] (InvalidValueDecodingErrorReason "Lower bound must not be greater than upper bound" (TextBuilder.toText (TextBuilder.decimal flags))))
-            _ -> pure ()
-        pure (BoundedRange (RangeBound lowerInclusive lowerValue) (RangeBound upperInclusive upperValue))
+
+        when (isJust lowerValue && not lowerInclusive) do
+          throwError (DecodingError ["lower-value"] (InvalidValueDecodingErrorReason "Lower bound cannot be exclusive when it is not infinite" (TextBuilder.toText (TextBuilder.decimal flags))))
+
+        when (isJust upperValue && upperInclusive) do
+          throwError (DecodingError ["upper-value"] (InvalidValueDecodingErrorReason "Upper bound cannot be inclusive" (TextBuilder.toText (TextBuilder.decimal flags))))
+
+        case (lowerValue, upperValue) of
+          (Just lv, Just uv) ->
+            when (lv >= uv) do
+              throwError (DecodingError ["upper-value"] (InvalidValueDecodingErrorReason "Upper value is smaller than the lower one" (TextBuilder.toText (TextBuilder.decimal flags))))
+          _ -> pure ()
+
+        pure (BoundedRange lowerValue upperValue)
     where
       decodeBound infinite =
         if infinite
@@ -84,38 +78,33 @@ instance (RangeMapping a, Ord a) => Mapping (Range a) where
 
   textualEncoder = \case
     EmptyRange -> "empty"
-    BoundedRange (RangeBound lowerInclusive lowerValue) (RangeBound upperInclusive upperValue) ->
+    BoundedRange lowerValue upperValue ->
       mconcat
-        [ if lowerInclusive then "[" else "(",
-          foldMap textualEncoder lowerValue,
+        [ case lowerValue of
+            Nothing -> "("
+            Just lowerValue -> "[" <> textualEncoder lowerValue,
           ",",
-          foldMap textualEncoder upperValue,
-          if upperInclusive then "]" else ")"
+          case upperValue of
+            Nothing -> ")"
+            Just upperValue -> textualEncoder upperValue <> ")"
         ]
 
-instance (Arbitrary a, Ord a, Enum a) => Arbitrary (Range a) where
+instance (Arbitrary a, Ord a) => Arbitrary (Range a) where
   arbitrary =
     QuickCheck.frequency
       [ (1, pure EmptyRange),
         ( 10,
           do
-            (lowerValue, upperValue) <- do
-              value1 <- QuickCheck.frequency [(1, pure Nothing), (10, Just <$> arbitrary)]
-              value2 <-
-                QuickCheck.frequency
-                  [ (1, pure Nothing),
-                    ( 10,
-                      Just <$> case value1 of
-                        Nothing -> arbitrary
-                        Just v -> QuickCheck.suchThat arbitrary (/= v)
-                    )
-                  ]
-              pure if value1 < value2 then (value1, value2) else (value2, value1)
-            lowerInclusive <- arbitrary
-            upperInclusive <-
-              if fmap succ lowerValue == upperValue && not lowerInclusive
-                then pure True
-                else arbitrary
-            pure (BoundedRange (RangeBound lowerInclusive lowerValue) (RangeBound upperInclusive upperValue))
+            value1 <- QuickCheck.frequency [(1, pure Nothing), (10, Just <$> arbitrary)]
+            value2 <-
+              QuickCheck.frequency
+                [ (1, pure Nothing),
+                  ( 10,
+                    Just <$> case value1 of
+                      Nothing -> arbitrary
+                      Just value1 -> QuickCheck.suchThat arbitrary (/= value1)
+                  )
+                ]
+            pure if value1 < value2 then BoundedRange value1 value2 else BoundedRange value2 value1
         )
       ]
