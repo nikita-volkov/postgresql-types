91acd375b786e56bb017d9a62b13f089047b5e41
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-08-22 14:57:50 +0300

    Refactor tests

--- src/integration-tests/Main.hs
+++ src/integration-tests/Main.hs
@@ -1,7 +1,6 @@
 module Main (main) where
 
-import Data.Proxy
-import qualified Main.Helpers
+import Main.Helpers
 import qualified PrimitiveLayer.Types as PrimitiveLayer
 import Test.Hspec
 import Test.QuickCheck.Instances ()
@@ -10,40 +9,40 @@ import Prelude
 main :: IO ()
 main =
   hspec do
-    aroundAll Main.Helpers.withPqConnection do
-      Main.Helpers.mappingSpec @PrimitiveLayer.Bit Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Bool Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Box Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Bytea Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Char Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Cidr Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Circle Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Date Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Float4 Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Float8 Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Inet Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Int2 Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Int4 Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Int8 Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Interval Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.IntervalAsMicroseconds Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Json Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Jsonb Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Line Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Lseg Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Macaddr Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Macaddr8 Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Money Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Numeric Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Oid Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Path Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Point Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Polygon Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Text Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Time Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Timestamp Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Timestamptz Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Timetz Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Uuid Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Varbit Proxy
-      Main.Helpers.mappingSpec @PrimitiveLayer.Varchar Proxy
+    aroundAll withPqConnection do
+      withType @PrimitiveLayer.Bit [mappingSpec]
+      withType @PrimitiveLayer.Bool [mappingSpec]
+      withType @PrimitiveLayer.Box [mappingSpec]
+      withType @PrimitiveLayer.Bytea [mappingSpec]
+      withType @PrimitiveLayer.Char [mappingSpec]
+      withType @PrimitiveLayer.Cidr [mappingSpec]
+      withType @PrimitiveLayer.Circle [mappingSpec]
+      withType @PrimitiveLayer.Date [mappingSpec]
+      withType @PrimitiveLayer.Float4 [mappingSpec]
+      withType @PrimitiveLayer.Float8 [mappingSpec]
+      withType @PrimitiveLayer.Inet [mappingSpec]
+      withType @PrimitiveLayer.Int2 [mappingSpec]
+      withType @PrimitiveLayer.Int4 [mappingSpec]
+      withType @PrimitiveLayer.Int8 [mappingSpec]
+      withType @PrimitiveLayer.Interval [mappingSpec]
+      withType @PrimitiveLayer.IntervalAsMicroseconds [mappingSpec]
+      withType @PrimitiveLayer.Json [mappingSpec]
+      withType @PrimitiveLayer.Jsonb [mappingSpec]
+      withType @PrimitiveLayer.Line [mappingSpec]
+      withType @PrimitiveLayer.Lseg [mappingSpec]
+      withType @PrimitiveLayer.Macaddr [mappingSpec]
+      withType @PrimitiveLayer.Macaddr8 [mappingSpec]
+      withType @PrimitiveLayer.Money [mappingSpec]
+      withType @PrimitiveLayer.Numeric [mappingSpec]
+      withType @PrimitiveLayer.Oid [mappingSpec]
+      withType @PrimitiveLayer.Path [mappingSpec]
+      withType @PrimitiveLayer.Point [mappingSpec]
+      withType @PrimitiveLayer.Polygon [mappingSpec]
+      withType @PrimitiveLayer.Text [mappingSpec]
+      withType @PrimitiveLayer.Time [mappingSpec]
+      withType @PrimitiveLayer.Timestamp [mappingSpec]
+      withType @PrimitiveLayer.Timestamptz [mappingSpec]
+      withType @PrimitiveLayer.Timetz [mappingSpec]
+      withType @PrimitiveLayer.Uuid [mappingSpec]
+      withType @PrimitiveLayer.Varbit [mappingSpec]
+      withType @PrimitiveLayer.Varchar [mappingSpec]
--- src/integration-tests/Main/Helpers.hs
+++ src/integration-tests/Main/Helpers.hs
@@ -58,47 +58,52 @@ withPqConnection action = do
             password = "postgres"
             db = "postgres"
 
+withType :: forall a. (Typeable a) => [Proxy a -> SpecWith Pq.Connection] -> SpecWith Pq.Connection
+withType specs = do
+  describe (show (typeOf (undefined :: a))) do
+    mapM_ (\spec -> spec Proxy) specs
+
 mappingSpec ::
   forall a.
-  (QuickCheck.Arbitrary a, Show a, Eq a, PrimitiveLayer.Mapping a, Typeable a) =>
+  (QuickCheck.Arbitrary a, Show a, Eq a, PrimitiveLayer.Mapping a) =>
   Proxy a ->
   SpecWith Pq.Connection
-mappingSpec _ = do
+mappingSpec _ = describe "Mapping" do
   let typeName = untag (PrimitiveLayer.typeName @a)
       baseOid = untag (PrimitiveLayer.baseOid @a)
       binEnc = PrimitiveLayer.binaryEncoder @a
       binDec = PrimitiveLayer.binaryDecoder @a
       txtEnc = PrimitiveLayer.textualEncoder @a
-  describe (show (typeOf (undefined :: a))) do
-    describe "Encoding via textualEncoder" do
-      describe "And decoding via binaryDecoder" do
-        it "Should produce the original value" \(connection :: Pq.Connection) ->
-          QuickCheck.property \(value :: a) -> do
-            QuickCheck.idempotentIOProperty do
-              bytes <-
-                runRoundtripQuery
-                  connection
-                  baseOid
-                  (Text.Encoding.encodeUtf8 (TextBuilder.toText (txtEnc value)))
-                  Pq.Text
-                  Pq.Binary
-              let decoding = PeekyBlinders.decodeByteStringDynamically binDec bytes
-              pure (decoding === Right (Right value))
 
-    describe "Encoding via binaryEncoder" do
-      describe "And decoding via binaryDecoder" do
-        it "Should produce the original value" \(connection :: Pq.Connection) ->
-          QuickCheck.property \(value :: a) -> do
-            QuickCheck.idempotentIOProperty do
-              bytes <-
-                runRoundtripQuery
-                  connection
-                  baseOid
-                  (PtrPoker.Write.writeToByteString (binEnc value))
-                  Pq.Binary
-                  Pq.Binary
-              let decoding = PeekyBlinders.decodeByteStringDynamically binDec bytes
-              pure (decoding === Right (Right value))
+  describe "Encoding via textualEncoder" do
+    describe "And decoding via binaryDecoder" do
+      it "Should produce the original value" \(connection :: Pq.Connection) ->
+        QuickCheck.property \(value :: a) -> do
+          QuickCheck.idempotentIOProperty do
+            bytes <-
+              runRoundtripQuery
+                connection
+                baseOid
+                (Text.Encoding.encodeUtf8 (TextBuilder.toText (txtEnc value)))
+                Pq.Text
+                Pq.Binary
+            let decoding = PeekyBlinders.decodeByteStringDynamically binDec bytes
+            pure (decoding === Right (Right value))
+
+  describe "Encoding via binaryEncoder" do
+    describe "And decoding via binaryDecoder" do
+      it "Should produce the original value" \(connection :: Pq.Connection) ->
+        QuickCheck.property \(value :: a) -> do
+          QuickCheck.idempotentIOProperty do
+            bytes <-
+              runRoundtripQuery
+                connection
+                baseOid
+                (PtrPoker.Write.writeToByteString (binEnc value))
+                Pq.Binary
+                Pq.Binary
+            let decoding = PeekyBlinders.decodeByteStringDynamically binDec bytes
+            pure (decoding === Right (Right value))
 
 runRoundtripQuery ::
   Pq.Connection ->
@@ -108,16 +113,39 @@ runRoundtripQuery ::
   Pq.Format ->
   IO ByteString.ByteString
 runRoundtripQuery connection paramOid paramEncoding paramFormat resultFormat = do
+  let params =
+        [ Just
+            ( paramOid,
+              paramEncoding,
+              paramFormat
+            )
+        ]
+  result <- runStatement connection "select $1" params resultFormat
+  bytes <- Pq.getvalue result 0 0
+  bytes <- case bytes of
+    Nothing -> fail "getvalue produced no bytes"
+    Just bytes -> pure bytes
+  pure bytes
+
+runStatement ::
+  Pq.Connection ->
+  Text.Text ->
+  -- | Params
+  [ Maybe
+      ( Word32,
+        ByteString.ByteString,
+        Pq.Format
+      )
+  ] ->
+  -- | Result format.
+  Pq.Format ->
+  IO Pq.Result
+runStatement connection sql params resultFormat = do
   result <-
     Pq.execParams
       connection
-      "select $1"
-      [ Just
-          ( Pq.Oid (fromIntegral paramOid),
-            paramEncoding,
-            paramFormat
-          )
-      ]
+      (Text.Encoding.encodeUtf8 sql)
+      (fmap (fmap (\(oid, encoding, format) -> (Pq.Oid (fromIntegral oid), encoding, format))) params)
       resultFormat
   result <- case result of
     Nothing -> do
@@ -128,8 +156,4 @@ runRoundtripQuery connection paramOid paramEncoding paramFormat resultFormat = d
   case resultErrorField of
     Nothing -> pure ()
     Just err -> fail ("Query failed: " <> show err)
-  bytes <- Pq.getvalue result 0 0
-  bytes <- case bytes of
-    Nothing -> fail "getvalue produced no bytes"
-    Just bytes -> pure bytes
-  pure bytes
+  pure result
