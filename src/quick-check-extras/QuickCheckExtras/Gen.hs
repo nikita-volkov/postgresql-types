da474e3fd05911847de974f1274e000b8382c21d
Nikita Volkov <nikita.y.volkov@mail.ru>
2025-08-25 12:44:05 +0300

    Extend QuickCheck extras

--- src/quick-check-extras/QuickCheckExtras/Gen.hs
+++ src/quick-check-extras/QuickCheckExtras/Gen.hs
@@ -2,7 +2,23 @@ module QuickCheckExtras.Gen where
 
 import qualified Data.Set as Set
 import Test.QuickCheck
+import Test.QuickCheck.Gen
+import qualified Test.QuickCheck.Random as Random
 import Prelude
+import Prelude hiding (choose, optional)
+
+-- * Execution
+
+run ::
+  Gen a ->
+  -- | Size.
+  Int ->
+  -- | Seed.
+  Int ->
+  a
+run gen size seed = unGen gen (Random.mkQCGen seed) size
+
+-- * Combinators
 
 setOfSize :: (Ord a) => Int -> Gen a -> Gen (Set.Set a)
 setOfSize n elementGen = go Set.empty
