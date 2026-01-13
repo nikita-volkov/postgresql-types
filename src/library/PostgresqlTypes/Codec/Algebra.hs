be8e6d25567ad432619d8f65e0d58c9d7674fe26
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-13 11:34:26 +0300

    Add the merged Primitive module

--- src/library/PostgresqlTypes/Codec/Algebra.hs
+++ src/library/PostgresqlTypes/Codec/Algebra.hs
@@ -5,8 +5,7 @@ module PostgresqlTypes.Codec.Algebra where
 import qualified Data.Vector as Vector
 import qualified PostgresqlTypes.Codec.Algebra.Writes as Writes
 import PostgresqlTypes.Codec.Prelude
-import qualified PostgresqlTypes.Primitive.Algebra as Primitive
-import qualified PostgresqlTypes.Primitive.Types as Primitive
+import qualified PostgresqlTypes.Primitive as Primitive
 import qualified PtrPeeker
 import qualified PtrPoker.Write as Write
 import qualified TextBuilder
---
+++ src/library/PostgresqlTypes/Primitive.hs
@@ -0,0 +1,10 @@
+module PostgresqlTypes.Primitive
+  ( module PostgresqlTypes.Primitive.Algebra,
+    module PostgresqlTypes.Primitive.Via,
+    module PostgresqlTypes.Primitive.Types,
+  )
+where
+
+import PostgresqlTypes.Primitive.Algebra
+import PostgresqlTypes.Primitive.Types
+import PostgresqlTypes.Primitive.Via
