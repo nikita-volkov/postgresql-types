88eeccced27dd7dabed181e8c0bbcf7b320305fa
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-14 14:12:18 +0300

    Extract the quickcheck-extras library

--- src/quick-check-extras/QuickCheckExtras/Arbitrary.hs
+++
@@ -1,8 +0,0 @@
-module QuickCheckExtras.Arbitrary where
-
-import qualified QuickCheckExtras.Gen as Gen
-import Test.QuickCheck
-import Prelude
-
-fromInt :: (Arbitrary a) => Int -> a
-fromInt = Gen.run arbitrary 10
