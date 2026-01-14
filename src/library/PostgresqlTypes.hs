cadf3967e508d5e8bea10353afecd9e79da2bc37
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-14 17:41:16 +0300

    Remove the reexports of Via

--- src/library/PostgresqlTypes.hs
+++ src/library/PostgresqlTypes.hs
@@ -1,10 +1,8 @@
 module PostgresqlTypes
   ( module PostgresqlTypes.Algebra,
-    module PostgresqlTypes.Via,
     module PostgresqlTypes.Types,
   )
 where
 
 import PostgresqlTypes.Algebra
 import PostgresqlTypes.Types
-import PostgresqlTypes.Via
