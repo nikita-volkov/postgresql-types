b7a52fb06a8326b9337c9dd517ca6442a94e02b2
Nikita Volkov <nikita.y.volkov@mail.ru>
2026-01-15 18:57:08 +0300

    Parallelize integration tests execution

--- src/integration-tests/Main/Helpers.hs
+++ src/integration-tests/Main/Helpers.hs
@@ -1,5 +1,7 @@
 module Main.Helpers where
 
+import Control.Concurrent.STM
+import Control.Exception
 import Control.Monad
 import qualified Data.Attoparsec.Text
 import qualified Data.ByteString as ByteString
@@ -11,6 +13,7 @@ import Data.Tagged
 import Data.Text (Text)
 import qualified Data.Text.Encoding as Text.Encoding
 import Data.Typeable
+import Data.Word
 import qualified Database.PostgreSQL.LibPQ as Pq
 import LawfulConversions
 import qualified PostgresqlTypes as PostgresqlTypes
@@ -24,64 +27,125 @@ import qualified TestcontainersPostgresql
 import qualified TextBuilder
 import Prelude
 
-withPqConnection :: Text -> Maybe Int -> SpecWith Pq.Connection -> Spec
-withPqConnection tagName extraFloatDigits =
-  describe testName . aroundAll executor
+withContainer :: Text -> SpecWith (Text, Word16) -> Spec
+withContainer tagName =
+  describe (to tagName) . aroundAll (TestcontainersPostgresql.run config)
   where
-    testName =
-      to tagName
-    executor action = do
-      TestcontainersPostgresql.run config \(host, port) -> do
-        connection <- Pq.connectdb (connectionString host port)
-        status <- Pq.status connection
-        case status of
-          Pq.ConnectionOk -> return ()
-          _ -> do
-            message <- Pq.errorMessage connection
-            fail ("Failed to connect to database: " <> show message)
-        _ <-
-          Pq.exec
-            connection
-            ( [ Just "SET client_min_messages TO WARNING",
-                Just "SET client_encoding = 'UTF8'",
-                Just "SET intervalstyle = 'iso_8601'",
-                Just "CREATE EXTENSION IF NOT EXISTS hstore",
-                fmap
-                  (\digits -> "SET extra_float_digits = " <> TextBuilder.decimal digits)
-                  extraFloatDigits
-              ]
-                & catMaybes
-                & fmap (<> ";")
-                & TextBuilder.intercalate "\n"
-                & TextBuilder.toText
-                & Text.Encoding.encodeUtf8
-            )
-        result <- action connection
-        Pq.finish connection
-        pure result
-      where
-        config =
-          TestcontainersPostgresql.Config
-            { forwardLogs = False,
-              auth = TestcontainersPostgresql.TrustAuth,
-              tagName
-            }
-        connectionString host port =
-          ByteString.intercalate " " components
-          where
-            components =
-              [ "host=" <> (Text.Encoding.encodeUtf8 host),
-                "port=" <> (fromString . show) port,
-                "user=" <> user,
-                "password=" <> password,
-                "dbname=" <> db
-              ]
-              where
-                user = "postgres"
-                password = "postgres"
-                db = "postgres"
+    config =
+      TestcontainersPostgresql.Config
+        { forwardLogs = False,
+          auth = TestcontainersPostgresql.TrustAuth,
+          tagName
+        }
 
-withType :: forall a. (Typeable a) => [Proxy a -> SpecWith Pq.Connection] -> SpecWith Pq.Connection
+withConnection :: Maybe Int -> SpecWith Pq.Connection -> SpecWith (Text, Word16)
+withConnection extraFloatDigits =
+  withConnectionPool 100 extraFloatDigits . withTQueueElement clean
+  where
+    clean connection = pure connection
+
+withConnectionPool :: Int -> Maybe Int -> SpecWith (TQueue Pq.Connection) -> SpecWith (Text, Word16)
+withConnectionPool poolSize extraFloatDigits =
+  describe name . mapSubject connectionStringByContainerInfo . withPool poolSize acquire release
+  where
+    name =
+      mconcat
+        [ "extraFloatDigits: ",
+          case extraFloatDigits of
+            Just digits -> show digits
+            Nothing -> "default"
+        ]
+
+    connectionStringByContainerInfo (host, port) =
+      ByteString.intercalate " " components
+      where
+        components =
+          [ "host=" <> (Text.Encoding.encodeUtf8 host),
+            "port=" <> (fromString . show) port,
+            "user=" <> user,
+            "password=" <> password,
+            "dbname=" <> db
+          ]
+          where
+            user = "postgres"
+            password = "postgres"
+            db = "postgres"
+
+    acquire connectionString = do
+      connection <- Pq.connectdb connectionString
+      status <- Pq.status connection
+      case status of
+        Pq.ConnectionOk -> pure ()
+        _ -> do
+          message <- Pq.errorMessage connection
+          fail ("Failed to connect to database: " <> show message)
+      result <-
+        Pq.exec
+          connection
+          ( [ Just "SET client_min_messages TO WARNING",
+              Just "SET client_encoding = 'UTF8'",
+              Just "SET intervalstyle = 'iso_8601'",
+              fmap
+                (\digits -> "SET extra_float_digits = " <> TextBuilder.decimal digits)
+                extraFloatDigits
+            ]
+              & catMaybes
+              & fmap (<> ";")
+              & TextBuilder.intercalate "\n"
+              & TextBuilder.toText
+              & Text.Encoding.encodeUtf8
+          )
+      case result of
+        Just _ -> pure ()
+        Nothing -> do
+          message <- Pq.errorMessage connection
+          fail ("Failed to set connection parameters: " <> show message)
+      pure connection
+
+    release = Pq.finish
+
+withPool ::
+  -- | Pool size.
+  Int ->
+  -- | Acquire.
+  (b -> IO a) ->
+  -- | Release.
+  (a -> IO ()) ->
+  SpecWith (TQueue a) ->
+  SpecWith b
+withPool poolSize acquire release =
+  describe ("poolSize: " <> show poolSize) . aroundAllWith \actionWithQueue b ->
+    bracket
+      ( do
+          queue <- newTQueueIO
+          replicateM_ poolSize do
+            element <- acquire b
+            atomically $ writeTQueue queue element
+          pure queue
+      )
+      ( \queue -> do
+          replicateM_ poolSize do
+            element <- atomically $ readTQueue queue
+            release element
+      )
+      actionWithQueue
+
+withTQueueElement ::
+  -- | Clean. Called upon returning to the pool.
+  (a -> IO a) ->
+  SpecWith a ->
+  SpecWith (TQueue a)
+withTQueueElement clean =
+  aroundWith \actionWithElement queue ->
+    bracket
+      (atomically $ readTQueue queue)
+      ( \element -> do
+          element <- clean element
+          atomically $ writeTQueue queue element
+      )
+      actionWithElement
+
+withType :: forall a b. (Typeable a) => [Proxy a -> SpecWith b] -> SpecWith b
 withType specs = do
   describe (show (typeOf (undefined :: a))) do
     mapM_ (\spec -> spec Proxy) specs
